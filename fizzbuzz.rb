7701f80bf986a2fbb2e4889baecbfe0780edcfc6
Olivia Macedo <liom62313@gmail.com>
2018-08-22 03:46:55 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz()
+def fizzbuzz(value)
+  answer = " "
+  if(value % 3 == 0 && value % 5 != 0)
+    answer = "Fizz"
+    return answer
+  elsif(value % 5 == 0 && value % 3 != 0)
+    answer = "Buzz"
+    return answer
+  elsif()
 
+  else
+
+  end
 end
